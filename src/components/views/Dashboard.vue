3a8c4d2016b94b7927f1ff135b950dea961dc952
tonhuduc1997@gmail.com <58973644+DucMystery@users.noreply.github.com>
2020-12-13 04:45:56 +0700

    push code setup

--- src/components/views/Dashboard.vue
+++ src/components/views/Dashboard.vue
@@ -2,53 +2,53 @@
   <!-- Main content -->
   <section class="content">
     <!-- GitHub hint -->
-    <div class="row">
-      <div class="col-xs-12">
-        <alert :dismissible="true"
-               type="success"
-               :iconClasses="['fa', 'fa-check']"
-               title="CoPilot is open source!">
-          <span>Click on icon to check it out on github.</span>
-          <a href="https://github.com/misterGF/CoPilot" target="_blank">
-            <i class="fa fa-github fa-2x"></i>
-          </a>
-        </alert>
-      </div>
+<!--    <div class="row">-->
+<!--      <div class="col-xs-12">-->
+<!--        <alert :dismissible="true"-->
+<!--               type="success"-->
+<!--               :iconClasses="['fa', 'fa-check']"-->
+<!--               title="CoPilot is open source!">-->
+<!--          <span>Click on icon to check it out on github.</span>-->
+<!--          <a href="https://github.com/misterGF/CoPilot" target="_blank">-->
+<!--            <i class="fa fa-github fa-2x"></i>-->
+<!--          </a>-->
+<!--        </alert>-->
+<!--      </div>-->
 
       <!-- Info boxes -->
       <div class="col-md-3 col-sm-6 col-xs-12">
-        <info-box color-class="bg-aqua"
-                  :icon-classes="['ion', 'ion-ios-gear-outline']"
-                  text="CPU Traffic"
-                  number="90%"></info-box>
+        <info-box color-class="bg-orange"
+                  :icon-classes="['fa', 'fa-facebook-official']"
+                  text="Bài đăng trên MXH"
+                  number="6888"></info-box>
       </div>
       <!-- /.col -->
       <div class="col-md-3 col-sm-6 col-xs-12">
-        <info-box color-class="bg-red"
-                  :icon-classes="['fa', 'fa-google-plus']"
-                  text="Likes"
-                  number="41,410"></info-box>
+        <info-box color-class="bg-green"
+                  :icon-classes="['ion', 'ion-ios-chatbubble-outline']"
+                  text="Bình luận trên MXH"
+                  number="8888666"></info-box>
       </div>
       <!-- /.col -->
 
       <!-- fix for small devices only -->
       <div class="clearfix visible-sm-block"></div>
-      
+
       <div class="col-md-3 col-sm-6 col-xs-12">
-        <info-box color-class="bg-green"
-                  :icon-classes="['ion', 'ion-ios-cart-outline']"
-                  text="Sales"
+        <info-box color-class="bg-red"
+                  :icon-classes="['fa', 'fa-newspaper-o']"
+                  text="Báo chí, Tin tức"
                   number="760"></info-box>
       </div>
       <!-- /.col -->
       <div class="col-md-3 col-sm-6 col-xs-12">
-        <info-box color-class="bg-yellow"
+        <info-box color-class="bg-aqua"
                   :icon-classes="['ion', 'ion-ios-people-outline']"
-                  text="New Members"
-                  number="2,000"></info-box>
+                  text="Nguồn khác(Diễn đàn, website...)"
+                  number="1000"></info-box>
       </div>
       <!-- /.col -->
-    </div>
+<!--    </div>-->
     <!-- /.row -->
 
     <div class="col-xs-12">
@@ -58,21 +58,18 @@
           <div class="box-body">
             <div class="col-sm-6 col-xs-12">
               <p class="text-center">
-                <strong>Web Traffic Overview</strong>
+                <strong>Số lượng nội dung có đề cập</strong>
               </p>
               <canvas id="trafficBar" ></canvas>
             </div>
             <hr class="visible-xs-block">
             <div class="col-sm-6 col-xs-12">
               <p class="text-center">
-                <strong>Language Overview</strong>
+                <strong>Tổng quan sắc thái</strong>
               </p>
-              <canvas id="languagePie"></canvas>
+              <canvas id="classification"></canvas>
             </div>
           </div>
-        </div>
-        <div class="text-center">
-          <small><b>Pro Tip</b> Don't forget to star us on github!</small>
         </div>
       </div>
     </div>
@@ -82,36 +79,36 @@
     <div class="row">
       <div class="col-md-3 col-sm-6 col-xs-12">
         <process-info-box color-class="bg-yellow"
-                          :icon-classes="['ion', 'ion-ios-pricetag-outline']"
-                          text="Inventory"
-                          number="5,200"
+                          :icon-classes="['fa', 'fa-facebook-official']"
+                          text="Số bài đăng nhắc tới từ khóa trên MXH"
+                          number="5,200/6888"
                           :progress="50"
-                          description="50% increase since May"></process-info-box>
+                          description="50% Trong tổng số bài đăng"></process-info-box>
       </div>
       <!-- /.col -->
       <div class="col-md-3 col-sm-6 col-xs-12">
         <process-info-box color-class="bg-green"
-                          :icon-classes="['ion', 'ion-ios-heart-outline']"
-                          text="Mentions"
-                          number="92,050"
+                          :icon-classes="['ion', 'ion-ios-chatbubble-outline']"
+                          text="Số bình luận MXH nhắc đến từ khóa"
+                          number="92,050/8888666"
                           :progress="20"
                           description="20% increase in 30 days"></process-info-box>
       </div>
       <!-- /.col -->
       <div class="col-md-3 col-sm-6 col-xs-12">
         <process-info-box color-class="bg-red"
-                          :icon-classes="['ion', 'ion-ios-cloud-download-outline']"
-                          text="Downloads"
-                          number="114,381"
+                          :icon-classes="['fa', 'fa-newspaper-o']"
+                          text="Số bài báo , tin tức nhắc đến từ kho"
+                          number="530/760"
                           :progress="70"
-                          description="70% increase since yesterday"></process-info-box>
+                          description="70% Tổng số bài báo, tin tức"></process-info-box>
       </div>
       <!-- /.col -->
       <div class="col-md-3 col-sm-6 col-xs-12">
         <process-info-box color-class="bg-aqua"
-                          :icon-classes="['ion', 'ion-ios-chatbubble-outline']"
-                          text="Direct Messages"
-                          number="163,921"
+                          :icon-classes="['ion', 'ion-ios-people-outline']"
+                          text="Số nội dung nhắc tới từ khóa trên diễn đàn, blog & website"
+                          number="400/1000"
                           :progress="40"
                           description="40% increase compared to last year"></process-info-box>
       </div>
@@ -147,11 +144,17 @@ export default {
     }
   },
   computed: {
-    coPilotNumbers () {
+    numberMXH () {
       return this.generateRandomNumbers(12, 1000000, 10000)
     },
-    personalNumbers () {
-      return this.generateRandomNumbers(12, 1000000, 10000)
+    numberComments () {
+      return this.generateRandomNumbers(12, 80000, 10000)
+    },
+    numberNewspaper () {
+      return this.generateRandomNumbers(12, 650000, 10000)
+    },
+    numberDifferent () {
+      return this.generateRandomNumbers(12, 4788888, 10000)
     },
     isMobile () {
       return (window.innerWidth <= 800 && window.innerHeight <= 600)
@@ -165,18 +168,30 @@ export default {
         data: {
           labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
           datasets: [{
-            label: 'CoPilot',
+            label: 'Bài đăng MXH',
             fill: false,
-            borderColor: '#284184',
-            pointBackgroundColor: '#284184',
+            borderColor: 'orange',
+            pointBackgroundColor: 'orange',
             backgroundColor: 'rgba(0, 0, 0, 0)',
-            data: this.coPilotNumbers
+            data: this.numberMXH
           }, {
-            label: 'Personal Site',
-            borderColor: '#4BC0C0',
-            pointBackgroundColor: '#4BC0C0',
+            label: 'Bình luận MXH',
+            borderColor: 'green',
+            pointBackgroundColor: 'green',
             backgroundColor: 'rgba(0, 0, 0, 0)',
-            data: this.personalNumbers
+            data: this.numberComments
+          }, {
+            label: 'Báo chí, Tin tức',
+            borderColor: 'red',
+            pointBackgroundColor: 'red',
+            backgroundColor: 'rgba(0, 0, 0, 0)',
+            data: this.numberNewspaper
+          }, {
+            label: 'Nguồn khác(diễn đàn, blog & website)',
+            borderColor: 'aqua',
+            pointBackgroundColor: 'aqua',
+            backgroundColor: 'rgba(0, 0, 0, 0)',
+            data: this.numberDifferent
           }]
         },
         options: {
@@ -197,13 +212,13 @@ export default {
 
       new Chart(ctx, config) // eslint-disable-line no-new
 
-      var pieChartCanvas = document.getElementById('languagePie').getContext('2d')
+      var pieChartCanvas = document.getElementById('classification').getContext('2d')
       var pieConfig = {
         type: 'pie',
         data: {
-          labels: ['HTML', 'JavaScript', 'CSS'],
+          labels: ['Tích cực', 'Tiêu cực', 'Trung tính'],
           datasets: [{
-            data: [56.6, 37.7, 4.1],
+            data: [40, 38, 22],
             backgroundColor: ['#00a65a', '#f39c12', '#00c0ef'],
             hoverBackgroundColor: ['#00a65a', '#f39c12', '#00c0ef']
           }]
